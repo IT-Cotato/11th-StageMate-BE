19ab4d0bee008ffcc06ced42fb1c7258a043011c
sehyunnnn <kkshyun56@gmail.com>
2025-06-27 16:35:35 +0900

    [init] : 프로젝트 파일 구조 구축

---
+++ src/main/java/com/example/stagemate/StagemateApplication.java
@@ -0,0 +1,13 @@
+package com.example.stagemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StagemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StagemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stagemate/StagemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stagemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StagemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
