f2d46fdd0041f8904f7c21a6ecd9b19bba9e1c05
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-07-02 08:15:40 +0900

    [feat] : ENUM을 DB에 저장하기위한 Converter 구현

---
+++ src/main/java/com/example/stagemate/global/converter/PerformanceStatusConverter.java
@@ -0,0 +1,24 @@
+package com.example.stagemate.global.converter;
+
+import com.example.stagemate.global.code.status.PerformanceStatus;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = false)
+public class PerformanceStatusConverter implements AttributeConverter<PerformanceStatus, String> {
+    @Override
+    public String convertToDatabaseColumn(PerformanceStatus performanceStatus) {
+        return performanceStatus.getDescription();
+    }
+
+    @Override
+    public PerformanceStatus convertToEntityAttribute(String dbData) {
+        if (dbData == null) {
+            return null;
+        }
+        String status = dbData.trim();
+
+        return PerformanceStatus.fromDescription(status);
+    }
+
+}
