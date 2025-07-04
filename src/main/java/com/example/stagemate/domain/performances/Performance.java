92fbb2b2e2616984f1ce76b9ca7e7220c144b0a9
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-07-05 07:29:17 +0900

    [feat] : Performance 관련 구현

---
+++ src/main/java/com/example/stagemate/domain/performances/Performance.java
@@ -0,0 +1,97 @@
+package com.example.stagemate.domain.performances;
+
+import com.example.stagemate.dto.data.CrawledPerformanceInfo;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDate;
+
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "performances")
+public class Performance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long performanceId; // 공연 ID
+
+    //인터파크에서 사용하는 item Id
+    private String interparkPerformanceId;
+
+    private String performanceName; // 공연 이름
+
+    private String url; // 공연 상세 URL
+
+    private String imageUrl; // 이미지 URL
+
+    private LocalDate startDate; // 시작 날짜
+
+    private LocalDate endDate; // 종료 날짜
+
+    private String theaterName; // 극장 이름
+
+    private String region; // 지역
+
+    @Enumerated(EnumType.STRING)
+    private PerformanceGenre performanceGenre;
+
+    @Enumerated(EnumType.STRING)
+    private PerformanceType performanceType; // 공연 유형 (뮤지컬, 연극 등)
+
+    @Enumerated(EnumType.STRING)
+    private PerformanceStatus performanceStatus; // 공연 상태 (예정, 진행중, 종료)
+
+    //현재시간 기준으로 상태업데이트
+    public void updateStatusBasedOnCurrentDate() {
+        LocalDate currentDate = LocalDate.now();
+
+        if (startDate.isBefore(currentDate) && endDate.isAfter(currentDate)) {
+            this.performanceStatus = PerformanceStatus.ONGOING;
+        } else if (endDate.isBefore(currentDate)) {
+            this.performanceStatus = PerformanceStatus.ENDED;
+        } else {
+            this.performanceStatus = PerformanceStatus.UPCOMING;
+        }
+    }
+
+    public void updateFromCrawledData(Performance crawled) {
+        this.interparkPerformanceId = crawled.getInterparkPerformanceId();
+        this.performanceName = crawled.getPerformanceName();
+        this.url = crawled.getUrl();
+        this.imageUrl = crawled.getImageUrl();
+        this.startDate = crawled.getStartDate();
+        this.endDate = crawled.getEndDate();
+        this.theaterName = crawled.getTheaterName();
+        this.region = crawled.getRegion();
+        this.performanceType = crawled.getPerformanceType();
+        this.performanceStatus = crawled.getPerformanceStatus();
+        this.performanceGenre = crawled.getPerformanceGenre();
+    }
+
+
+
+    //crawledPerformanceInfo -> Performance
+    public static Performance from(CrawledPerformanceInfo crawledPerformanceInfo) {
+        return Performance.builder()
+                .interparkPerformanceId(crawledPerformanceInfo.getInterparkPerformanceId())
+                .performanceName(crawledPerformanceInfo.getPerformanceName())
+                .url(crawledPerformanceInfo.getPerformanceUrl())
+                .imageUrl(crawledPerformanceInfo.getImageUrl())
+                .startDate(crawledPerformanceInfo.getStartDate())
+                .endDate(crawledPerformanceInfo.getEndDate())
+                .theaterName(crawledPerformanceInfo.getTheaterName())
+                .region(crawledPerformanceInfo.getRegion())
+                .performanceType(crawledPerformanceInfo.getPerformanceType())
+                .performanceStatus(crawledPerformanceInfo.getPerformanceStatus())
+                .performanceGenre(crawledPerformanceInfo.getPerformanceGenre())
+                .build();
+    }
+
+}
--- src/main/java/com/example/stagemate/domain/performances/PerformanceStatus.java
+++ src/main/java/com/example/stagemate/domain/performances/PerformanceStatus.java
@@ -1,26 +1,10 @@
 package com.example.stagemate.domain.performances;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
 public enum PerformanceStatus {
-    UPCOMING("상영예정"),
-    ONGOING("상영중"),
-    ENDED("상영종료");
-
-    private final String description;
-
-    public static PerformanceStatus fromDescription(String description) {
-        for (PerformanceStatus status : PerformanceStatus.values()) {
-            if (status.getDescription().equals(description)) {
-                return status;
-            }
-        }
-        return null; //exception 처리 필요
-    }
-
+    UPCOMING,
+    ONGOING,
+    ENDED;
 
 
 }
