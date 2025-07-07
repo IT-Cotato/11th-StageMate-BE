03929e6ab18d5e0befc725ab1f5f956056260d1d
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-07-07 19:21:18 +0900

    [feat] : Archive 구현

---
+++ src/main/java/com/example/stagemate/dto/request/ArchiveCreateRequest.java
@@ -0,0 +1,17 @@
+package com.example.stagemate.dto.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@RequiredArgsConstructor
+public class ArchiveCreateRequest {
+    private LocalDate viewingDate;
+    private String casting;
+    private String review;
+    private double rating;
+    private String memo;
+    private String imageUrl;
+}
