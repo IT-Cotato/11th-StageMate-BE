20b439a9f74af12f9ed7256672561643a61b5295
he-on <hyeonjub2911@g.hongik.ac.kr>
2025-07-08 05:45:58 +0900

    feat: Spring Security 및 세션 설정

--- src/main/java/com/example/stagemate/global/auth/SecurityConfig.java
+++ src/main/java/com/example/stagemate/global/auth/SecurityConfig.java
@@ -1,4 +1,68 @@
 package com.example.stagemate.global.auth;
 
+import com.example.stagemate.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(
+                        (csrfConfig) -> csrfConfig.disable()
+                )
+                .headers(
+                        (headerConfig) -> headerConfig.frameOptions(
+                                frameOptionsConfig -> frameOptionsConfig.disable()
+                        )
+                )
+                .authorizeHttpRequests((authorizeRequest) -> authorizeRequest
+                        .requestMatchers(
+                                "/", "/test/", "/css/**", "/images/**", "/js/**",
+                                "/login", "/login/**",
+                                "/logout", "/logout/**",
+                                "/oauth2/**",
+                                "/login/oauth2/authorization/**",
+                                "/api/v1/auth/**",
+                                "/swagger-ui/**", "/v3/api-docs/**",
+                                "/posts/**", "/comments/**"
+                        ).permitAll()
+                        .requestMatchers("/posts/new", "/comments/save").hasRole(Role.USER.name())
+                        .anyRequest().authenticated()
+                )
+                .logout( // 로그아웃 성공 시 / 주소로 이동
+                        (logoutConfig) -> logoutConfig.logoutSuccessUrl("/")
+                )
+                // OAuth2 로그인 기능에 대한 여러 설정
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")  // 이 경로는 컨트롤러에서 뷰로 연결하거나 정적 페이지여도 무방
+                        .authorizationEndpoint(authorization -> authorization
+                                .baseUri("/login/oauth2/authorization")
+                        )
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService))
+                );
+
+        return http.build();
+    }
 }
--- src/main/java/com/example/stagemate/global/security/session/SessionManager.java
+++ src/main/java/com/example/stagemate/global/security/session/SessionManager.java
@@ -1,4 +1,58 @@
 package com.example.stagemate.global.security.session;
 
+import com.example.stagemate.dto.auth.GuestInfo;
+import com.example.stagemate.global.exception.AppException;
+import com.example.stagemate.global.exception.CommonErrorCode;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class SessionManager {
+
+    private static final String EMAIL = "email";
+    private static final String SESSION_USER_ID = "SESSION_USER_ID";
+    private static final String GUEST_INFO = "GUEST_INFO";
+
+    private final HttpSession session;
+    private final HttpServletRequest request;
+
+    public void login(String email) {
+        HttpSession session = request.getSession();
+        session.setAttribute(EMAIL, email);
+    }
+
+    public void logout() {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+    }
+
+    public String getSessionUserId() {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return null;
+        }
+        return (String) session.getAttribute(SESSION_USER_ID);
+    }
+
+    public void setGuestInfo(GuestInfo guestInfo) {
+        HttpSession session = request.getSession(true);
+        session.setAttribute(GUEST_INFO, guestInfo);
+    }
+
+    public GuestInfo getGuestInfo() {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            throw new AppException(CommonErrorCode.UNAUTHORIZED, "세션이 존재하지 않습니다.");
+        }
+        Object guestInfo = session.getAttribute(GUEST_INFO);
+        if (!(guestInfo instanceof GuestInfo)) {
+            throw new AppException(CommonErrorCode.UNAUTHORIZED, "세션에 게스트 정보가 없습니다.");
+        }
+        return (GuestInfo) guestInfo;
+    }
 }
