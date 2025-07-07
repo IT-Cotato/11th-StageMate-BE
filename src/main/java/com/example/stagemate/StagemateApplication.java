176efd6790fcb561abfc542dfcf0bf4678938890
he-on <hyeonjub2911@g.hongik.ac.kr>
2025-07-08 06:37:39 +0900

    restore: drop된 전역 설정 및 유틸 파일 복구

--- src/main/java/com/example/stagemate/StagemateApplication.java
+++ src/main/java/com/example/stagemate/StagemateApplication.java
@@ -2,11 +2,13 @@ package com.example.stagemate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 //스케줄링 기능을 활성화하기 위한 어노테이션
 @EnableScheduling
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.stagemate.repository")
 public class StagemateApplication {
 
 	public static void main(String[] args) {
