9c82d82e7b50945f5a8fe76b973df9867bb6b578
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-07-07 19:22:09 +0900

    [feat] : ScheduleReport 구현

---
+++ src/main/java/com/example/stagemate/domain/scheduleReport/ScheduleReportCategory.java
@@ -0,0 +1,29 @@
+package com.example.stagemate.domain.scheduleReport;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "schedule_report_categorys")
+public class ScheduleReportCategory {
+    @jakarta.persistence.Id
+    @jakarta.persistence.Column(name = "schedule_report_category_id")
+    private Long id;
+
+    @ManyToOne
+    private ScheduleReport scheduleReport;
+
+    private ScheduleReportCategoryType scheduleReportCategoryType;
+
+    private int categoryOrder;
+
+}
