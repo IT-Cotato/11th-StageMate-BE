5a9cd1e9b3d3239e45ebe80e4b54ac99d1d20942
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-07-08 20:48:02 +0900

    [feat] : spring validation으로 검증구현

--- src/main/java/com/example/stagemate/controller/ArchiveController.java
+++ src/main/java/com/example/stagemate/controller/ArchiveController.java
@@ -2,8 +2,13 @@ package com.example.stagemate.controller;
 
 import com.example.stagemate.domain.archive.Archive;
 import com.example.stagemate.dto.request.ArchiveCreateRequest;
+import com.example.stagemate.dto.request.ArchiveUpdateRequest;
 import com.example.stagemate.global.dto.DataResponse;
 import com.example.stagemate.service.archive.ArchiveService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,27 +17,39 @@ import org.springframework.web.bind.annotation.*;
 public class ArchiveController {
     private final ArchiveService archiveService;
 
+    @Tag(name = "Archive", description = "Archive API")
+    @Operation(summary = "아카이브 상세 정보", description = "아카이브 상세 정보를 가져옴")
+    @ApiResponse(responseCode = "200", description = "아카이브 상세 정보를 가져옴")
     @GetMapping("/api/v1/archive/{archiveId}")
     public DataResponse<Archive> getArchive(@PathVariable Long archiveId) {
         return DataResponse.from(archiveService.getArchive(archiveId));
     }
 
+    @Tag(name = "Archive", description = "Archive API")
+    @Operation(summary = "아카이브 생성", description = "아카이브 생성")
+    @ApiResponse(responseCode = "200", description = "아카이브 생성")
     @PostMapping("/api/v1/archive")
-    public DataResponse<Long> createArchive(@RequestBody ArchiveCreateRequest archiveCreateRequest) {
+    public DataResponse<Long> createArchive(@Valid @RequestBody ArchiveCreateRequest archiveCreateRequest) {
         return DataResponse.from(archiveService.createArchive(Archive.create(archiveCreateRequest)));
     }
 
+    @Tag(name = "Archive", description = "Archive API")
+    @Operation(summary = "아카이브 삭제", description = "아카이브 삭제")
+    @ApiResponse(responseCode = "200", description = "아카이브 삭제")
     @DeleteMapping("/api/v1/archive/{archiveId}")
     public DataResponse<Void> deleteArchive(@PathVariable Long archiveId) {
         archiveService.deleteArchive(archiveId);
         return DataResponse.ok();
     }
 
-    //archive Update Reqeust 필요
-//    @PutMapping("/api/v1/archive/{archiveId}")
-//    public DataResponse<?> updateArchive(@PathVariable Long archiveId, Archive archive) {
-//        archiveService.updateArchive(archive);
-//        return DataResponse.ok();
-//    }
+//    archive Update Reqeust 필요
+    @Tag(name = "Archive", description = "Archive API")
+    @Operation(summary = "아카이브 변경", description = "아카이브 변경")
+    @ApiResponse(responseCode = "200", description = "아카이브 변경")
+    @PutMapping("/api/v1/archive/{archiveId}")
+    public DataResponse<?> updateArchive(@PathVariable Long archiveId, @Valid @RequestBody ArchiveUpdateRequest archiveUpdateRequest) {
+        archiveService.updateArchive(archiveId, archiveUpdateRequest);
+        return DataResponse.ok();
+    }
 
 }
--- src/main/java/com/example/stagemate/controller/ScheduleReportController.java
+++ src/main/java/com/example/stagemate/controller/ScheduleReportController.java
@@ -8,6 +8,7 @@ import com.example.stagemate.service.scheduleReport.ScheduleReportService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,33 +20,33 @@ public class ScheduleReportController {
     private final ScheduleReportService scheduleReportService;
 
     @Tag(name = "ScheduleReport", description = "ScheduleReport API")
-    @Operation(summary = "스케줄 리포트 상세 정보", description = "스케줄 리포트 상세 정보를 가져옴")
-    @ApiResponse(responseCode = "200", description = "스케줄 리포트 상세 정보를 가져옴")
+    @Operation(summary = "공식스케줄 리포트 상세 정보", description = "공식스케줄리포트 상세 정보를 가져옴")
+    @ApiResponse(responseCode = "200", description = "공식일정리포트 상세 정보를 가져옴")
     @GetMapping("/api/v1/scheduleReport/{scheduleReportId}")
     public DataResponse<ScheduleReport> getScheduleReport(@PathVariable Long scheduleReportId) {
         return DataResponse.from(scheduleReportService.getScheduleReport(scheduleReportId));
     }
 
     @Tag(name = "ScheduleReport", description = "ScheduleReport API")
-    @Operation(summary = "스케줄 생성", description = "스케줄 생성")
-    @ApiResponse(responseCode = "200", description = "스케줄 생성")
+    @Operation(summary = "공식스케줄리포트 생성", description = "공식스케줄리포트 생성")
+    @ApiResponse(responseCode = "200", description = "공식스케줄리포트 생성")
     @PostMapping("/api/v1/scheduleReport")
     public DataResponse<Long> createScheduleReport(
-            @RequestBody ScheduleReportCreateRequest scheduleReportCreateRequest) {
+            @Valid @RequestBody ScheduleReportCreateRequest scheduleReportCreateRequest) {
         return DataResponse.from(scheduleReportService.createScheduleReport(scheduleReportCreateRequest));
     }
 
     @Tag(name = "ScheduleReport", description = "ScheduleReport API")
-    @Operation(summary = "스케줄 상태별 조회", description = "스케줄 상태별 조회")
-    @ApiResponse(responseCode = "200", description = "스케줄 상태별 조회")
+    @Operation(summary = "공식스케줄리포트 상태별 조회", description = "공식스케줄리포트 상태별 조회, 주로 관리자가 PENDING 상태인 공식스케줄리포트를 확인하는 용도")
+    @ApiResponse(responseCode = "200", description = "공식스케줄리포트 상태별 조회")
     @GetMapping("/api/v1/scheduleReport")
     public DataResponse<List<ScheduleReport>> getScheduleReports(@RequestParam ScheduleReportStatus scheduleReportStatus) {
         return DataResponse.from(scheduleReportService.getScheduleReports(scheduleReportStatus));
     }
 
     @Tag(name = "ScheduleReport", description = "ScheduleReport API")
-    @Operation(summary = "스케줄 상태 변경", description = "스케줄 상태 변경")
-    @ApiResponse(responseCode = "200", description = "스케줄 상태 변경")
+    @Operation(summary = "공식스케줄리포트 상태 변경", description = "공식스케줄리포트 상태 변경, 관리자가 PENDING 상태인 공식스케줄리포트를 APPROVED, REJECTED 상태로 바꾸는 용도")
+    @ApiResponse(responseCode = "200", description = "공식스케줄리포트 상태 변경")
     @PutMapping("/api/v1/scheduleReport/{scheduleReportId}")
     public DataResponse<ScheduleReport> changeScheduleReportStatus(
             @PathVariable Long scheduleReportId, @RequestParam ScheduleReportStatus scheduleReportStatus) {
