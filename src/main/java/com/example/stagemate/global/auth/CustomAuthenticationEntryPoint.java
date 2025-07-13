ecbe07437a26503185d1cc5ec39276bfc7f922ab
he-on <hyeonjub2911@g.hongik.ac.kr>
2025-07-14 02:05:37 +0900

    [feat] : JWT 기반 인증 구성 추가

---
+++ src/main/java/com/example/stagemate/global/auth/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,40 @@
+package com.example.stagemate.global.auth;
+
+
+import com.example.stagemate.global.dto.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static com.example.stagemate.global.exception.CommonErrorCode.UNAUTHORIZED;
+
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+
+        ErrorResponse errorResponse = ErrorResponse.of(UNAUTHORIZED, request);
+
+        response.setStatus(UNAUTHORIZED.getHttpStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+    }
+}
