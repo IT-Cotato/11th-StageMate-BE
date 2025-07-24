f19b5d8d388603148949a215ec2b0ad2e3751003
he-on <hyeonjub2911@g.hongik.ac.kr>
2025-07-24 23:31:25 +0900

    fix: 회원가입 순서를 정보 제공 동의 -> 정보 입력으로 변경

--- src/main/java/com/example/stagemate/service/user/RegisterUserUseCase.java
+++ src/main/java/com/example/stagemate/service/user/RegisterUserUseCase.java
@@ -8,7 +8,9 @@ import com.example.stagemate.service.user.command.RegisterUserCommand;
 public interface RegisterUserUseCase {
     String normalSignupInfo(RegisterUserCommand command);
 
+    // 동의 정보까지 포함한 최종 회원가입 (OAuth 등에서 사용)
     User normalAgreeAndRegister(NormalAgreeCommand command, String userId);
 
-    void execute(RegisterUserRequestDTO request);
+    // 일반 회원가입 (정보입력 + 유저 생성)
+    User execute(RegisterUserRequestDTO request); // 리턴 타입 변경
 }
