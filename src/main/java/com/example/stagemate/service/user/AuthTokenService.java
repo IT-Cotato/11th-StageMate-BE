73c58ccfd43547449b9c9dc0fb2ec9c1f016b027
he-on <hyeonjub2911@g.hongik.ac.kr>
2025-07-30 23:33:12 +0900

    refactor: 토큰 저장 로직 AuthTokenService로 분리

--- src/main/java/com/example/stagemate/service/user/AuthTokenService.java
+++ src/main/java/com/example/stagemate/service/user/AuthTokenService.java
@@ -1,4 +1,35 @@
 package com.example.stagemate.service.user;
 
+
+import com.example.stagemate.domain.user.entity.RefreshTokenEntity;
+import com.example.stagemate.dto.response.TokenResponseDTO;
+import com.example.stagemate.global.auth.JwtTokenProvider;
+import com.example.stagemate.repository.user.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
 public class AuthTokenService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public TokenResponseDTO generateTokensAndSave(Long userId) {
+        String accessToken = jwtTokenProvider.createToken(userId);
+        String refreshToken = jwtTokenProvider.createRefreshToken(userId);
+
+        refreshTokenRepository.save(
+                RefreshTokenEntity.builder()
+                        .userId(userId)
+                        .token(refreshToken)
+                        .expiresAt(LocalDateTime.now().plusDays(14))
+                        .build()
+        );
+
+        return new TokenResponseDTO(accessToken, refreshToken);
+    }
+
 }
