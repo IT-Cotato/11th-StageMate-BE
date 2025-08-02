12d2f35fd82f0fa38640578e1f4acf3aeee583c1
임준서 <limjunseo123123@gmail.com>
2025-08-02 19:33:32 +0900

    [feat] : 추천공연 장르태그 추가

--- src/main/java/com/example/stagemate/dto/response/performance/RecommendedPerformanceResponse.java
+++ src/main/java/com/example/stagemate/dto/response/performance/RecommendedPerformanceResponse.java
@@ -1,14 +1,18 @@
 package com.example.stagemate.dto.response.performance;
 
 import com.example.stagemate.domain.performance.Performance;
+import com.example.stagemate.domain.performance.PerformanceGenre;
 import com.example.stagemate.domain.performance.PerformanceStatistics;
 
 public record RecommendedPerformanceResponse(
         PerformanceDetailResponse performanceDetailResponse,
+        PerformanceGenre performanceGenre,
         Long increasedScrapCount
 ) {
     public static RecommendedPerformanceResponse from(Performance performance, PerformanceStatistics performanceStatistics) {
         return new RecommendedPerformanceResponse(
-                PerformanceDetailResponse.from(performance), performanceStatistics.getIncreasedScrapCount());
+                PerformanceDetailResponse.from(performance),
+                performance.getPerformanceGenre(),
+                performanceStatistics.getIncreasedScrapCount());
     }
 }
