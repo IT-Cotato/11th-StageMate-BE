72db489fc6cb9a433fa6c26ab3a4f1cb43bf1b01
limjunseo <nonsultop1@g.hongik.ac.kr>
2025-08-07 20:20:19 +0900

    [feat] : swagger cors 설정

--- src/main/java/com/example/stagemate/global/config/SwaggerConfig.java
+++ src/main/java/com/example/stagemate/global/config/SwaggerConfig.java
@@ -1,35 +1,53 @@
 package com.example.stagemate.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
+
 @Configuration
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(
+                title = "StageMate API",
+                version = "v1.0.0",
+                description = "스테이지메이트 API 목록입니다."
+        ),
+        servers = {
+                @io.swagger.v3.oas.annotations.servers.Server(
+                        url = "https://api.stagemate.co.kr", description = "Production Server"),
+                @io.swagger.v3.oas.annotations.servers.Server(
+                        url = "http://api.stagemate.co.kr", description = "HTTP Server"),
+                @io.swagger.v3.oas.annotations.servers.Server(
+                        url = "http://localhost:8080", description = "Local Server")
+        }
+)
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
-        final String securitySchemeName = "bearerAuth";  // 스웨거에서 사용할 인증 방식 이름
-
-        Info info = new Info()
-                .version("v1.0.0")
-                .title("StageMate API")
-                .description("스테이지메이트 API 목록입니다.");
+        final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
-                .info(info)
-                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName)) // 전역 보안 적용
-                .components(new Components()
-                        .addSecuritySchemes(securitySchemeName,
-                                new SecurityScheme()
-                                        .name(securitySchemeName)
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        )
-                );
+                .info(new Info().title("StageMate API").version("v1.0.0").description("스테이지메이트 API 목록입니다."))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components().addSecuritySchemes(securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ))
+                .servers(List.of(
+                        new Server().url("https://api.stagemate.co.kr").description("Production Server"),
+                        new Server().url("http://api.stagemate.co.kr").description("HTTP Server"),
+                        new Server().url("http://localhost:8080").description("Local Server")
+                ));
     }
 }
